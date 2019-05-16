80c46741553f4d65d946c5f2f8bf5ad45478e621
Dan Connolly <dckc@madmode.com>
2019-05-16 14:21:51 -0500

    StudyServer wraps StudyTeam.team in HTTP server

--- src/main/groovy/kumc_bmi/StudyTeam.groovy
+++ src/main/groovy/kumc_bmi/StudyTeam.groovy
@@ -9,12 +9,18 @@ import java.sql.SQLFeatureNotSupportedException
 import java.util.logging.Logger
 import javax.sql.DataSource
 
+import com.sun.net.httpserver.HttpExchange
+
 import groovy.json.JsonOutput
 import groovy.sql.Sql
 import groovy.transform.CompileStatic
+import groovy.transform.Immutable
 
+@Immutable
 @CompileStatic
 class StudyTeam {
+    private static Logger logger = Logger.getLogger("")
+
     public static void main(String[] args) {
         def fail = { ex -> System.err.println(ex); System.exit(1) }
         def dbAccess = { String u, Properties p -> DriverManager.getConnection(u, p) }
@@ -36,6 +42,15 @@ class StudyTeam {
                 byId(src, id, out)
             }
         } catch (java.sql.SQLException ex) { fail(ex) }
+
+        if (cli.flag("--serve")) {
+            def fallback = { d -> { s -> s == null ? d : s } }
+            String host = fallback("127.0.0.1")(config.getProperty("http.host"))
+
+            def port = fallback("8080")(config.getProperty("http.port")) as int
+            logger.info("serving at http://$host:$port")
+            new StudyServer(src: src, socketAddress: host, port: port).start()
+        }
     }
 
     static Properties getConfig(File configFile) {
@@ -46,13 +61,13 @@ class StudyTeam {
         properties
     }
 
-    static def byId(DataSource src, String id, PrintStream out) {
+    static void byId(DataSource src, String id, PrintStream out) {
         out.println("Members of study with ID=$id:")
         def info = team(src, id)
         out.println(JsonOutput.prettyPrint(JsonOutput.toJson(info)))
     }
 
-    static def team(DataSource src, String id) {
+    static List team(DataSource src, String id) {
         def sql = new Sql(src)
         def q = """
             select p.ID as [Employee ID], p.userId, p.accountDisabled
@@ -69,11 +84,15 @@ class StudyTeam {
 }
 
 
+@Immutable
 @CompileStatic
 class CLI {
     String[] args
 
-    def flag = { args.contains(it) }
+    boolean flag(String it) {
+        args.contains(it)
+    }
+
     def arg(String sentinel, Closure thunk) {
         def i = (args as List).findIndexOf { it == sentinel }
         def l = args.length
@@ -91,6 +110,7 @@ class CLI {
  * @param name prefix to distinguish among sets of connection properties;
  *             e.g. `db1.url = ...`, `db2.url = ...`.
  */
+@Immutable
 @CompileStatic
 class DBConfig extends NoopDataSource {
     Properties config
@@ -161,4 +181,43 @@ abstract class NoopDataSource implements DataSource {
         throw new SQLException()
     }
 
+}
+
+
+/** HTTP service to look up IRB study team info by study id.
+ *
+ * @param port server listening port
+ * @param src [[Connector]] to e-compliance DB.
+ */
+@CompileStatic
+class StudyServer extends SimpleHttpServerBase {
+    private Logger logger = Logger.getLogger("")
+
+    DataSource src
+
+    /** Handle requests for study team info.
+     *
+     * For GET requests with a query string in the format ?id=XXX,
+     * consult the src DB and respond with study team info in
+     * JSON format.
+     *
+     * Other requests result in an HTTP 404 reponse.
+     *
+     * @see [[StudyTeam.team]]
+     */
+    void handle(HttpExchange exchange) {
+        String query = exchange.getRequestURI().getQuery()
+
+        def m = query =~ /id=(.*)/
+        if (m.find()) {
+            String id = m.group(1)
+            logger.info("GET id=$id")
+            List info = StudyTeam.team(src, id)
+            logger.info("protocol $id has ${info.size()} study team members")
+            def text = JsonOutput.prettyPrint(JsonOutput.toJson(info))
+            respond(exchange, 200, text)
+        } else {
+            respond(exchange, 404, "")
+        }
+    }
 }
