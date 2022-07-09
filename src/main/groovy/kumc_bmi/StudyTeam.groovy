5aee3c4b49630787aa8060c2fa5954b2a13e5f56
sa-ssatyakam <sa-ssatyakam@bmi-util-0.kumc.edu>
2022-07-09 09:24:44 -0500

    src/main/groovy/kumc_bmi/StudyTeam.groovy: set logger for the class CLI to get errors for explicit constructor

--- src/main/groovy/kumc_bmi/StudyTeam.groovy
+++ src/main/groovy/kumc_bmi/StudyTeam.groovy
@@ -101,7 +101,7 @@ class CLI {
     CLI(args_given) {
       def raw_args = Eval.me(args_given.toString())
       assert raw_args.class == String[]  
-      logger.warn(raw_args)
+      logger.warning(raw_args)
       this.args = raw_args
     }
 
